89d8ca0c432919aca791cad230a3aa76420d97a2
chenw <760022342@qq.com>
2021-11-01 11:07:12 +0800

    sass

--- src/pages/SpreadDemo.tsx
+++ src/pages/SpreadDemo.tsx
@@ -6,6 +6,7 @@ import {
 } from "@grapecity/spread-sheets-react";
 import { Tabs } from "antd";
 import { FC, useEffect, useState } from "react";
+import "../assets/css/spread.scss";
 import { spreadDate } from "../data/spread";
 // GC.Spread.Sheets.LicenseKey = 'sds';
 const { TabPane } = Tabs;
@@ -14,8 +15,8 @@ const SpreadDemo: FC = () => {
   const [spreadBackColor, setSpreadBackColor] = useState("#fff");
   const [sheetName, setSheetName] = useState("Goods List");
   const [hostStyle, setHostStyle] = useState<any>({
-    width: "800px",
-    height: "600px",
+    width: "100%",
+    height: "100%",
   });
   const [columnWidth, setColumnWidth] = useState(100);
   const [data, setData] = useState(spreadDate);
@@ -24,36 +25,35 @@ const SpreadDemo: FC = () => {
   useEffect(() => {});
 
   return (
-    <div style={{ margin: "100px" }}>
-      <Tabs
-        defaultActiveKey={activeIndex}
-        onChange={(key) => setActiveIndex(key)}
-        type="card"
-      >
-        <TabPane tab="Tab 1" key="1">
-          <SpreadSheets backColor={spreadBackColor} hostStyle={hostStyle}>
-            <Worksheet name={sheetName} dataSource={data}>
-              <Column dataField="Name" width={300}></Column>
-              <Column dataField="Category" width={columnWidth}></Column>
-              <Column
-                dataField="Price"
-                width={columnWidth}
-                formatter="$#.00"
-              ></Column>
-              <Column dataField="Shopping Place" width={columnWidth}></Column>
-            </Worksheet>
-          </SpreadSheets>
-        </TabPane>
-        <TabPane tab="Tab 2" key="2">
-          <SpreadSheets backColor={spreadBackColor} hostStyle={hostStyle}>
-            <Worksheet name={sheetName} dataSource={data}>
-              <Column dataField="Name" width={300}></Column>
-              <Column dataField="Category" width={columnWidth}></Column>
-            </Worksheet>
-          </SpreadSheets>
-        </TabPane>
-      </Tabs>
-    </div>
+    <Tabs
+      className="tab-content"
+      defaultActiveKey={activeIndex}
+      onChange={(key) => setActiveIndex(key)}
+      type="card"
+    >
+      <TabPane tab="Tab 1" key="1" className="height100">
+        <SpreadSheets backColor={spreadBackColor} hostStyle={hostStyle}>
+          <Worksheet name={sheetName} dataSource={data}>
+            <Column dataField="Name" width={300}></Column>
+            <Column dataField="Category" width={columnWidth}></Column>
+            <Column
+              dataField="Price"
+              width={columnWidth}
+              formatter="$#.00"
+            ></Column>
+            <Column dataField="Shopping Place" width={columnWidth}></Column>
+          </Worksheet>
+        </SpreadSheets>
+      </TabPane>
+      <TabPane tab="Tab 2" key="2" className="height100">
+        <SpreadSheets backColor={spreadBackColor} hostStyle={hostStyle}>
+          <Worksheet name={sheetName} dataSource={data}>
+            <Column dataField="Name" width={300}></Column>
+            <Column dataField="Category" width={columnWidth}></Column>
+          </Worksheet>
+        </SpreadSheets>
+      </TabPane>
+    </Tabs>
   );
 };
 
