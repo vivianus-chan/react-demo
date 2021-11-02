2e2b58adbe567f3585a74f94b9a09025414e208d
chenw <760022342@qq.com>
2021-11-02 17:08:07 +0800

    汉化

--- src/components/ExcelSheet.tsx
+++ src/components/ExcelSheet.tsx
@@ -1,38 +1,86 @@
+import GC from "@grapecity/spread-sheets";
 import {
   Column,
   SpreadSheets,
   Worksheet
 } from "@grapecity/spread-sheets-react";
-import { spreadData } from "data/spread";
-import { FC, useEffect, useState } from "react";
-// import GC from '@grapecity/spread-sheets';
-// GC.Spread.Sheets.LicenseKey = 'sds';
+import { useEffect, useState } from "react";
 
-export const ExcelSheet: FC<IExcelSheetProps> = (props) => {
-  const [spreadBackColor, setSpreadBackColor] = useState("#fff");
+export const ExcelSheet: React.FC<IExcelSheetProps> = (props) => {
+  const [spread, setSpread] = useState(null);
   const [sheetName, setSheetName] = useState("假装数据");
   const [hostStyle, setHostStyle] = useState<any>({
-    width: "100%",
-    height: "100%",
+    width: "80%",
+    height: "80%",
+    margin: "30px auto",
   });
-  const [columnWidth, setColumnWidth] = useState(100);
-  const [data, setData] = useState<any>(spreadData);
 
   useEffect(() => {
-    console.log(props);
-  }, [props.data]);
+    console.log(props, spread);
+  }, []);
+
+  const initSpread = (spread: any) => {
+    setSpread(spread);
+    spread.suspendPaint();
+    let sheet = spread.getActiveSheet();
+    // sheet.setRowHeight(col, 147);
+    // sheet.setColumnWidth(col, 147);
+    // console.log(spread.fromJSON(json))
+    var spreadNS = GC.Spread.Sheets;
+    // spread.setSheetCount(3);
+    // var self = this;
+    // spread.bind(spreadNS.Events.ActiveSheetChanged, function (e, args) {
+    //   var index = spread.getActiveSheetIndex();
+    //   self.activeSheetIndex = index;
+    // });
+    //   spread.bind(spreadNS.Events.CellClick, function (e, args) {
+    //     let sheetArea = args.sheetArea === 0 ? 'sheetCorner' : args.sheetArea === 1 ? 'columnHeader' : args.sheetArea === 2 ? 'rowHeader' : 'viewPort';
+    //     let log =
+    //         'SpreadEvent: ' + GC.Spread.Sheets.Events.CellClick + ' event called' + '\n' +
+    //         'sheetArea: ' + sheetArea + '\n' +
+    //         'row: ' + args.row + '\n' +
+    //         'col: ' + args.col;
+    //     self.setState({ eventLog: log });
+    // });
+    //   spread.bind(spreadNS.Events.EditStarting, function (e, args) {
+    //     let log =
+    //         'SpreadEvent: ' + GC.Spread.Sheets.Events.EditStarting + ' event called' + '\n' +
+    //         'row: ' + args.row + '\n' +
+    //         'column: ' + args.col;
+    //     self.setState({ eventLog: log });
+    // });
+    // spread.bind(spreadNS.Events.EditEnded, function (e, args) {
+    //     let log =
+    //         'SpreadEvent: ' + GC.Spread.Sheets.Events.EditEnded + ' event called' + '\n' +
+    //         'row: ' + args.row + '\n' +
+    //         'column: ' + args.col + '\n' +
+    //         'text: ' + args.editingText;
+    //     self.setState({ eventLog: log });
+    // });
+    spread.resumePaint();
+  };
 
   return (
-    <SpreadSheets backColor={spreadBackColor} hostStyle={hostStyle}>
-      <Worksheet name={sheetName} dataSource={data}>
+    <SpreadSheets
+      backColor="#fff"
+      // grayAreaBackColor="#E4E4E4"
+      hostStyle={hostStyle}
+      newTabVisible={false}
+      tabStripVisible={true}
+      scrollbarMaxAlign={true}
+      workbookInitialized={(spread) => initSpread(spread)}
+    >
+      <Worksheet
+        name={sheetName}
+        dataSource={props.data}
+        autoGenerateColumns={false}
+        // frozenRowCount={1}
+        // frozenColumnCount={1}
+      >
         <Column dataField="Name" width={300}></Column>
-        <Column dataField="Category" width={columnWidth}></Column>
-        <Column
-          dataField="Price"
-          width={columnWidth}
-          formatter="$#.00"
-        ></Column>
-        <Column dataField="Shopping Place" width={columnWidth}></Column>
+        <Column dataField="Category" width={100}></Column>
+        <Column dataField="Price" width={100} formatter="$#.00"></Column>
+        <Column dataField="Shopping Place" width={100}></Column>
       </Worksheet>
     </SpreadSheets>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
-import '@grapecity/spread-sheets/styles/gc.spread.sheets.excel2013white.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import GC from "@grapecity/spread-sheets";
+import "@grapecity/spread-sheets-resources-zh";
+import "@grapecity/spread-sheets/styles/gc.spread.sheets.excel2013white.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+// GC.Spread.Sheets.LicenseKey = 'sds';
+GC.Spread.Common.CultureManager.culture("zh-cn");
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
